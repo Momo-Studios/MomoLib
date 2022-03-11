c1f26f10eb9505d9743e4490779da28dc648aeea
iMikul <gamer3mc@gmail.com>
2022-03-11 14:23:37 -0500

    Remove test registry

--- src/main/java/dev/momostudios/momolib/MomoLib.java
+++ src/main/java/dev/momostudios/momolib/MomoLib.java
@@ -1,9 +1,5 @@
 package dev.momostudios.momolib;
 
-import dev.momostudios.momolib.util.MMRegistryBuilder;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -17,9 +13,5 @@ public class MomoLib
     public MomoLib()
     {
         MinecraftForge.EVENT_BUS.register(this);
-
-        MMRegistryBuilder.newRegistry(Block.class, "momolib")
-                .add("testblock",     new Block(AbstractBlock.Properties.create(Material.CLAY).hardnessAndResistance(0.3f, 1)))
-                .add("testblock_two", new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1, 6)));
     }
 }
